baab958a8a7675cba7142543b67fd4380e303857
SlimDaddy228 <60612282+SlimDaddy228@users.noreply.github.com>
2020-02-29 00:26:34 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,5 +1,4 @@
 local Proxy = module("vrp", "lib/Proxy")
-
 vRP = Proxy.getInterface("vRP")
 
 local function ch_dog_sit(player,choice)
@@ -42,3 +41,119 @@ vRP.registerMenuBuilder("main", function(add, data)
     add(choices)
   end
 end)
+
+
+-- Build item menu rottweiler
+local ch_dog_rottweiler = {}  
+ch_dog_rottweiler["Заспавнить/удалить собаку"] = {function(player,choice)
+  local user_id = vRP.getUserId(player) -- get user_id
+  if user_id then
+      TriggerClientEvent("vrp_dog:spawndog_rottweiler",player)
+      vRP.closeMenu(player)
+    end
+end,"Милая собачка"}
+
+ch_dog_rottweiler["Лежать"] = {function(player,choice) -- 
+  local user_id = vRP.getUserId(player) -- get user_id
+  if user_id then
+      TriggerClientEvent("vrp_dog:laydown",player)
+      vRP.closeMenu(player) 
+    end
+end}
+
+ch_dog_rottweiler["Сидеть"] = {function(player,choice) -- 
+  local user_id = vRP.getUserId(player) -- get user_id
+  if user_id then
+      TriggerClientEvent("vrp_dog:sit",player)
+      vRP.closeMenu(player) 
+    end
+end}
+
+-- add item definition
+vRP.defInventoryItem("dog_rottweiler","Ротвейлер","<img src='https://i.imgur.com/l9Xyi8g.png' height='200' width='200' />",function() return ch_dog_rottweiler end,0.5)
+
+--Build item menu husky
+
+local ch_dog_a_c_husky = {}  
+ch_dog_a_c_husky["Заспавнить/удалить собаку"] = {function(player,choice)
+  local user_id = vRP.getUserId(player) -- get user_id
+  if user_id then
+      TriggerClientEvent("vrp_dog:spawndog_husky",player)
+      vRP.closeMenu(player)
+    end
+end,"Милая собачка"}
+
+ch_dog_a_c_husky["Лежать"] = {function(player,choice) -- 
+  local user_id = vRP.getUserId(player) -- get user_id
+  if user_id then
+      TriggerClientEvent("vrp_dog:laydown",player)
+      vRP.closeMenu(player) 
+    end
+end}
+
+ch_dog_a_c_husky["Сидеть"] = {function(player,choice) -- 
+  local user_id = vRP.getUserId(player) -- get user_id
+  if user_id then
+      TriggerClientEvent("vrp_dog:sit",player)
+      vRP.closeMenu(player) 
+    end
+end}
+
+vRP.defInventoryItem("dog_husky","Хаски","<img src='https://i.imgur.com/U4ko0PK.png' height='200' width='200' />",function() return ch_dog_a_c_husky end,0.5)
+
+--Build item menu
+local ch_dog_shepherd = {}  
+ch_dog_shepherd["Заспавнить/удалить собаку"] = {function(player,choice)
+  local user_id = vRP.getUserId(player) -- get user_id
+  if user_id then
+      TriggerClientEvent("vrp_dog:spawndog_shepherd",player)
+      vRP.closeMenu(player)
+    end
+end,"Милая собачка"}
+
+ch_dog_shepherd["Лежать"] = {function(player,choice) -- 
+  local user_id = vRP.getUserId(player) -- get user_id
+  if user_id then
+      TriggerClientEvent("vrp_dog:laydown",player)
+      vRP.closeMenu(player) 
+    end
+end}
+
+ch_dog_shepherd["Сидеть"] = {function(player,choice) -- 
+  local user_id = vRP.getUserId(player) -- get user_id
+  if user_id then
+      TriggerClientEvent("vrp_dog:sit",player)
+      vRP.closeMenu(player) 
+    end
+end}
+
+vRP.defInventoryItem("dog_shepherd","Дворняжная собака","<img src='https://i.imgur.com/5khxQgd.png' height='200' width='200' />",function() return ch_dog_shepherd end,0.5)
+
+--Build item menu retriever
+
+local ch_dog_retriever = {}  
+ch_dog_retriever["Заспавнить/удалить собаку"] = {function(player,choice)
+  local user_id = vRP.getUserId(player) -- get user_id
+  if user_id then
+      TriggerClientEvent("vrp_dog:spawndog_retriever",player)
+      vRP.closeMenu(player)
+    end
+end,"Милая собачка"}
+
+ch_dog_retriever["Лежать"] = {function(player,choice) -- 
+  local user_id = vRP.getUserId(player) -- get user_id
+  if user_id then
+      TriggerClientEvent("vrp_dog:laydown",player)
+      vRP.closeMenu(player) 
+    end
+end}
+
+ch_dog_retriever["Сидеть"] = {function(player,choice) -- 
+  local user_id = vRP.getUserId(player) -- get user_id
+  if user_id then
+      TriggerClientEvent("vrp_dog:sit",player)
+      vRP.closeMenu(player) 
+    end
+end}
+
+vRP.defInventoryItem("dog_retriever","Дворняжка","<img src='https://i.imgur.com/295yW9s.png' height='200' width='200' />",function() return ch_dog_retriever end,0.5)
